c46e9824feed76f2ae592d48344878a8bcd00004
Joy <joykenneth605@gmail.com>
2026-06-09 17:03:16 +0100

    feat(route): add health route

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,9 +1,14 @@
 import express from "express";
 import jobsRoutes from "./modules/jobs/jobs.route.js";
+import { StatusCodes } from "http-status-codes";
 
 export default function routes(app) {
     app.use(express.json());
     app.use(express.urlencoded({extended: true})); 
 
     app.use('/jobs', jobsRoutes);
+
+    app.get('/health', (req, res) => {
+        res.status(StatusCodes.OK).json({ status: 'ok', uptime: process.uptime() });
+    });
 }
